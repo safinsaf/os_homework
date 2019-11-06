7417ecadb129dfaadc03c65a9878095892ef0212
safinsaf <safinsaft@gmail.com>
2019-11-07 01:09:21 +0300

    2.0

--- week12/ex1.c
+++ week12/ex1.c
@@ -3,16 +3,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define N 100
+#define N 20
 int main(){
 	int a = open("/dev/random", O_RDONLY);
 
-	for (int i = 0; i < 20; i++){
-		char temp[N];
-		int b = read(a, temp, N);
-		for (int i = 0; i < N; i++){
-			printf("%c", (temp[i]+1000)%70 + 32);
-		}
-		printf("\n");
+	char temp[N];
+	int b = read(a, temp, N);
+	for (int i = 0; i < N; i++){
+		printf("%c", (temp[i]+1000)%70 + 32);
 	}
+	printf("\n");
 }
